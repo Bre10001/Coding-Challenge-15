c6099c593afc097b49b48e77e2e9b52758944f2a
Breanna Burris <breanna1burris@gmail.com>
2024-11-07 16:36:56 -0500

    portfolio mod w 2 functions.

---
+++ portfolio.js
@@ -0,0 +1,22 @@
+// Task 2: Create the Portfolio Module
+
+// Import named exports
+import { assets } from './asset.js';
+
+// Define and export function
+
+export function calculatePortfolioValue() {
+    return assets.reduce((total, asset) => total + asset.price * asset.quantity, 0);
+}
+
+// Export function
+
+export function getPortfolioAllocation() {
+    const totalValue = calculatePortfolioValue();
+    return assets.map(asset => ({
+        name: asset.name,
+        allocation: ((asset.price * asset.quantity) / totalValue) * 100
+    }));
+}
+
+
