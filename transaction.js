e962bb97e09200cfae9131f31b7200cdb9120299
Breanna Burris <breanna1burris@gmail.com>
2024-11-10 16:46:39 -0500

    Create transaction module with Transaction class.

---
+++ transaction.js
@@ -0,0 +1,25 @@
+// Task 3: Create the Transaction Module
+
+// Import named exports
+import { assets, getAssetById } from './asset.js';
+
+//Define and export transaction class
+
+export class Transaction {
+    constructor(assetId, type, quantity) {
+        this.assetId = assetId;
+        this.type = type;
+        this.quantity = quantity;
+
+        const asset = getAssetById(assetId);
+        if (type === 'sell' && asset.quantity < quantity) {
+            throw new Error(`Insufficient quantity for sale of ${asset.name}`);
+        }
+
+        if (type === 'buy') {
+            asset.quantity += quantity;
+        } else if (type === 'sell') {
+            asset.quantity -= quantity;
+        }
+    }
+}
